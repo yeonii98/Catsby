73bbfc91cceb1c933d02b6f68654b70fdebf1e24
zzin0616 <zzin0616@naver.com>
2021-07-12 03:47:15 +0900

    AndroidManifest, QRscan

--- app/src/main/java/org/techtown/catsby/QRcode/qrcodeActivity.java
+++ app/src/main/java/org/techtown/catsby/QRcode/qrcodeActivity.java
@@ -1,22 +1,23 @@
 package org.techtown.catsby.QRcode;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.widget.TextView;
 
 import org.techtown.catsby.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class QRbutton extends AppCompatActivity {
+public class qrcodeActivity extends AppCompatActivity {
 
     Button btnScan;
     String[] permissions = {
@@ -27,13 +28,17 @@ public class QRbutton extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_qrcode);
+
+        Intent intent = getIntent();
+        double latitude = intent.getDoubleExtra("latitude", 0);
+        double longitude = intent.getDoubleExtra("longitude", 0);
 
         btnScan = (Button) findViewById(R.id.button);
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), QRscanner.class));
+                startActivity(new Intent(getApplicationContext(), scannerActivity.class));
             }
         });
 
@@ -52,4 +57,6 @@ public class QRbutton extends AppCompatActivity {
         }
         return true;
     }
+
 }
+
