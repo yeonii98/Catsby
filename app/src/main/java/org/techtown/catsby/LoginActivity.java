07711c33466831856968dd25d8846db0c10ab09d
yejiin <yj_song67@naver.com>
2021-07-12 17:09:36 +0900

    Add kakao login

--- app/src/main/java/org/techtown/catsby/LoginActivity.java
+++ app/src/main/java/org/techtown/catsby/LoginActivity.java
@@ -1,27 +1,10 @@
 package org.techtown.catsby;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.internal.OnConnectionFailedListener;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -30,17 +13,37 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
 import org.jetbrains.annotations.NotNull;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
+
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String TAG = "LoginActivity";
+
     private SignInButton btn_google;
     private FirebaseAuth auth;
     private GoogleApiClient googleApiClient;
@@ -53,10 +56,14 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     private LoginButton buttonFacebook;
     private boolean loginSuccess;
 
+    private ImageView buttonKakao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        KakaoSdk.init(this, getString(R.string.kakao_native_app_key));
 
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -109,6 +116,19 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
         });
 
+        buttonKakao = (ImageView) findViewById(R.id.btn_kakao_login);
+        buttonKakao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
+                    UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this, kakaoCallback());
+                } else {
+                    UserApiClient.getInstance().loginWithKakaoAccount(LoginActivity.this, kakaoCallback());
+                }
+            }
+        });
+
+        updateKakaoLoginUi();
     }
 
     @Override
@@ -123,8 +143,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 resultLogin(account);
             }
         }
-
-
     }
 
     private void resultLogin(GoogleSignInAccount account){
@@ -146,7 +164,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 });
 
     }
-
 
     // 페이스북 로그인 이벤트
     // 사용자가 정상적으로 로그인한 후 페이스북 로그인 버튼의 onSuccess 콜백 메소드에서 로그인한 사용자의
@@ -182,5 +199,35 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     @Override
     public void onConnectionFailed(@NonNull @NotNull ConnectionResult connectionResult) {
 
+    }
+
+    @NotNull
+    private Function2<OAuthToken, Throwable, Unit> kakaoCallback() {
+        return new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                if (throwable != null) {
+                    // 로그인 실패시
+                    Log.e(TAG, throwable.getMessage());
+                    Toast.makeText(getApplication(), "로그인 실패", Toast.LENGTH_LONG).show();
+
+                }
+                updateKakaoLoginUi();
+                return null;
+            }
+        };
+    }
+
+    private void updateKakaoLoginUi() {
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                if (user != null) {
+                    Intent intent = new Intent(getApplication(), MainActivity.class);
+                    startActivity(intent);
+                }
+                return null;
+            }
+        });
     }
 }
