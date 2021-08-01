e090d42504a7445ad66bfe4728d543cba6785a4d
zzin0616 <zzin0616@naver.com>
2021-08-02 04:27:17 +0900

    .

--- Android/app/src/main/java/org/techtown/catsby/community/FragmentCommunity.java
+++ Android/app/src/main/java/org/techtown/catsby/community/FragmentCommunity.java
@@ -1,13 +1,14 @@
 package org.techtown.catsby.community;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -20,6 +21,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.techtown.catsby.R;
 
@@ -31,12 +34,16 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FragmentCommunity extends Fragment implements View.OnClickListener {
+public class FragmentCommunity extends Fragment {
 
     private View view;
     private TextView tv_frag1;
-    private ImageButton btn_move;
+    private Button btn_move;
     private String result;
+    private RecyclerView recyclerView;
+    private RecyclerAdapter recyclerAdapter;
+
+    List<Memo> memoList;
 
     @Nullable
     @Override
@@ -44,6 +51,30 @@ public class FragmentCommunity extends Fragment implements View.OnClickListener 
                              @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_community, container, false);
+
+        memoList = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recyclerview);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerAdapter = new RecyclerAdapter(memoList);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        //새로운 메모 작성
+        btn_move = view.findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("fromFrag1", ".");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                FragmentPost fragment2 = new FragmentPost();
+                fragment2.setArguments(bundle);
+                transaction.replace(R.id.frameLayout, fragment2);
+                transaction.commit();
+            }
+        });
 
         tv_frag1 = view.findViewById(R.id.tv_frag1);
         btn_move = view.findViewById(R.id.btn_move);
@@ -67,6 +98,7 @@ public class FragmentCommunity extends Fragment implements View.OnClickListener 
             }
         });
 
+        //검색
         SearchView searchView = view.findViewById(R.id.search_view);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -83,9 +115,62 @@ public class FragmentCommunity extends Fragment implements View.OnClickListener 
         return view;
     }
 
+    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
 
-    @Override
-    public void onClick(View view) {
+        private List<Memo> listdata;
 
+        public RecyclerAdapter(List<Memo> listdata) {
+            this.listdata = listdata;
+        }
+
+
+        @NonNull
+        @Override
+        public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,
+                    viewGroup, false);
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public int getItemCount() {
+            return listdata.size();
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i) {
+            Memo memo = listdata.get(i);
+
+            itemViewHolder.maintext.setText(memo.getMaintext());
+            itemViewHolder.subtext.setText(memo.getSubtext());
+
+            if (memo.getIsdone() == 0) {
+                itemViewHolder.img.setBackgroundColor(Color.LTGRAY);
+            } else {
+                itemViewHolder.img.setBackgroundColor(Color.GREEN);
+            }
+        }
+
+        void addItem(Memo memo) {
+            listdata.add(memo);
+        }
+
+        void removeItem(int position) {
+            listdata.remove(position);
+        }
+
+        class ItemViewHolder extends RecyclerView.ViewHolder {
+            private TextView maintext;
+            private TextView subtext;
+            private ImageView img;
+
+            public ItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                maintext = itemView.findViewById(R.id.item_maintext);
+                subtext = itemView.findViewById(R.id.item_subtext);
+                img = itemView.findViewById(R.id.item_image);
+            }
+        }
     }
 }
