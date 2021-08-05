b1dc7d9f4e220ab45997419664bd94fb61d612da
wotj102@gmail.com <wotj102@gmail.com>
2021-08-05 21:56:44 +0900

    깃랩 업데이트

--- Server/src/main/java/com/hanium/catsby/TownComment/repository/TownCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hanium.catsby.TownComment.repository;
-
-import com.hanium.catsby.domain.TownComment;
-import com.hanium.catsby.domain.TownLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TownCommentRepository extends JpaRepository<TownComment, Integer> {
-}
--- Server/src/main/java/com/hanium/catsby/TownCommunity/repository/TownCommunityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hanium.catsby.TownCommunity.repository;
-
-import com.hanium.catsby.domain.TownCommunity;
-import com.hanium.catsby.domain.TownLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TownCommunityRepository extends JpaRepository<TownCommunity, Integer> {
-}
